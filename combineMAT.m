6a8bb1ef1bab8530a7d2a71743b11cc363bbd199
huanghe141327 <45162028+huanghe141327@users.noreply.github.com>
2020-08-04 16:30:36 +0800

    Create combineMAT.m

---
+++ combineMAT.m
@@ -0,0 +1,17 @@
+filepath='/pub/work/huoyf/FY/';
+x1=load('/pub/work/huoyf/FY/FY201901.mat');
+f=fieldnames(x1.FY);
+num1=[1:18,23:25];
+num2=[19:22];
+for j=2:12
+    file=dir([filepath,'FY2019',num2str(j,'%.2d'),'.mat']);
+    filename=[filepath,file.name];
+    x2=load(filename);
+for i =1:size(num1,2)
+    x1.FY.(f{num1(i)})=cat(1,x1.FY.(f{num1(i)}),x2.FY.(f{num1(i)}));
+end
+
+for i =1:size(num2,2)
+    x1.FY.(f{num2(i)})=cat(2,x1.FY.(f{num2(i)}),x2.FY.(f{num2(i)}));
+end
+end
